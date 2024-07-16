256b7db2e115aa2e89742456effe6d5ed61f046d
Sabarish7113 <sabarish.user@gmail.com>
2024-07-16 16:38:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+//initialize
+// function initialize(){
+// let a =[10,20,30,40,50]
+// document.write(a)
+// }
+// initialize()
+
+//sort
+// function sorting()
+// {
+//     let a =[20,30,10,50,40]
+//     b=a.sort()
+//     document.write(b)
+// }
+// sorting()
+
+//reverse
+// function sorting()
+// {
+//     let a =[10,20,30,40,50]
+//     b=a.reverse()
+//     document.write(b)
+// }
+// sorting()
+
+//spread
+// function spread()
+// {
+//     let a = [60,70,80,90,100]
+//     let b = [10,20,30,40,50,...a]
+//     document.write(b)
+// }
+// spread()
+
+//range
+// function range()
+// {
+//     let a =11
+//     let b = []
+//     for(let i=0;i<a;i++)
+//     {
+//         b.push(i)
+//     }
+//     console.log(b)
+// }
+// range()
+
+
+
+
+
